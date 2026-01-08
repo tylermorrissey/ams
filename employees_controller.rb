6f7fd4b9a3c0b11b879534253fe925cb5a1509e8
Jacob <jacoblittler2003@gmail.com>
2026-01-07 19:16:08 -0500

    Fixed enviornment

---
+++ employees_controller.rb
@@ -0,0 +1,57 @@
+class EmployeesController < ApplicationController
+  before_action :set_employee, only: [:show, :edit, :update, :destroy]
+
+  # GET /employees
+  def index
+    @employees = Employee.all
+  end
+
+  # GET /employees/1
+  def show
+  end
+
+  # GET /employees/new
+  def new
+    @employee = Employee.new
+  end
+
+  # GET /employees/1/edit
+  def edit
+  end
+
+  # POST /employees
+  def create
+    @employee = Employee.new(employee_params)
+
+    if @employee.save
+      redirect_to @employee, notice: "Employee was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /employees/1
+  def update
+    if @employee.update(employee_params)
+      redirect_to @employee, notice: "Employee was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /employees/1
+  def destroy
+    @employee.destroy
+    redirect_to employees_url, notice: "Employee was successfully deleted."
+  end
+
+  private
+
+  def set_employee
+    @employee = Employee.find(params[:id])
+  end
+
+  def employee_params
+    params.require(:employee).permit(:name, :email, :phone, :role, :hire_date)
+  end
+end
