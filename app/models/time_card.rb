715d1db77ac3d80b2bbf46b0fba13d6873885acb
Jacob <jacoblittler2003@gmail.com>
2026-02-24 11:06:00 -0500

    Added validations and validations tests

--- app/models/time_card.rb
+++ app/models/time_card.rb
@@ -1,6 +1,9 @@
 class TimeCard < ApplicationRecord
   belongs_to :project
   belongs_to :employee
+
+  validates :clock_in, presence: true
+  validates :clock_out, presence: true
 end
 
 def hours_worked(start, finish)
