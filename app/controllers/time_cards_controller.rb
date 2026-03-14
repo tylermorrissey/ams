f06b2e4e305a6978ef1c59ecde9c1b34693c02dc
Jacob <jacoblittler2003@gmail.com>
2026-03-13 22:00:45 -0400

    syntax

--- app/controllers/time_cards_controller.rb
+++ app/controllers/time_cards_controller.rb
@@ -1,5 +1,5 @@
 class TimeCardsController < ApplicationController
-  before_action :set_time_card, only: [:show, :edit, :update, :destroy]
+  before_action :set_time_card, only: %i[show edit update destroy]
 
   def new
     @time_card = TimeCard.new
@@ -9,7 +9,7 @@ class TimeCardsController < ApplicationController
     @time_card = TimeCard.create(time_card_params)
 
     if @time_card.save
-      redirect_to @time_card, notice: "Time Card was successfully recorded" 
+      redirect_to @time_card, notice: 'Time Card was successfully recorded'
     else
       render :new, status: :unprocessable_entity
     end
@@ -27,7 +27,7 @@ class TimeCardsController < ApplicationController
 
   def update
     if @time_card.update(time_card_params)
-      redirect_to @time_card, notice: "Time Card was updated"
+      redirect_to @time_card, notice: 'Time Card was updated'
     else
       render :edit, status: :unprocessable_entity
     end
@@ -35,10 +35,11 @@ class TimeCardsController < ApplicationController
 
   def destroy
     @time_card.destroy
-    redirect_to time_cards_path, notice: "Time Card was successfully deleted."
+    redirect_to time_cards_path, notice: 'Time Card was successfully deleted.'
   end
 
   private
+
   def set_time_card
     @time_card = TimeCard.find(params[:id])
   end
